6a01c769a5820d78745c3e0b40e07e9f298001be
pawan.yadav <pawan.yadav@algosec.com>
2025-10-05 12:25:03 +0530

    initial commit for git infar set up, docker based infra

---
+++ src/main/java/com/learning/awsinfra/AwsInfraApplication.java
@@ -0,0 +1,13 @@
+package com.learning.awsinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsInfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/awsinfra/AwsInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.awsinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
